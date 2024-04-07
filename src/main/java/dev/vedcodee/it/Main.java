c76bb57cc9dc0fe06b00878ff91590b03ba6ea03
scienziato1pazzo <141784142+scienziatopazzo@users.noreply.github.com>
2024-04-07 22:01:32 +0200

    First Commit

---
+++ src/main/java/dev/vedcodee/it/Main.java
@@ -0,0 +1,17 @@
+package dev.vedcodee.it;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+       
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
